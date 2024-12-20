f2214b2208ab03843f94418aad271e248a82cf35
Leslie <leslie_aldaz@hotmail.com>
2024-12-20 16:05:54 -0700

    create boot mvc project, link JPA project dependency

---
+++ CookieCounter/src/main/java/com/skilldistillery/cookie/CookieCounterApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.cookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieCounterApplication.class, args);
+	}
+
+}
---
+++ CookieCounter/src/main/java/com/skilldistillery/cookie/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.cookie;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CookieCounterApplication.class);
+	}
+
+}
